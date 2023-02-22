0c51a576221c0659cb07f75513535b2c0c884725
Alejandro Ching <alejandroching2004@hotmail.com>
2023-02-22 10:40:29 -0500

    Adding styles to UserDetailsExtended

--- src/components/UserDetailsExtended.js
+++ src/components/UserDetailsExtended.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
-
-
+import './../styles/UserDetailsView.css';
 
 
 function UserDetailsExtended() {
@@ -38,29 +37,40 @@ function UserDetailsExtended() {
     }, [login]);
 
     return (
-        <div>
+        <div className="user-details-extended">
             <h2>{userData.name}</h2>
             <img src={userData.avatar_url} alt="avatar" />
             <p>{userData.bio}</p>
             <p>{userData.location}</p>
             <p>{userData.email}</p>
             <p>{userData.company}</p>
-            <h3>Repositories:</h3>
-            <ul>
-                {userRepos.map(repo => (
-                    <li key={repo.id}>
-                        <a href={repo.html_url}>{repo.name}</a>
-                    </li>
-                ))}
-            </ul>
-            <h3>Organizations:</h3>
-            <ul>
-                {userOrgs.map(org => (
-                    <li key={org.id}>
-                        <a href={org.html_url}>{org.login}</a>
-                    </li>
-                ))}
-            </ul>
+            <div className="user-details-extended__repos">
+                <h3>Repositories:</h3>
+                <ul>
+                    {userRepos.map(repo => (
+                        <li key={repo.id}>
+                            <div className="repo-item">
+                                <a href={repo.html_url} className="repo-item__name">{repo.name}</a>
+                                <div className="repo-item__info">
+                                    <p>{repo.description}</p>
+                                    <p>Language: {repo.language}</p>
+                                    <p>Stars: {repo.stargazers_count}</p>
+                                </div>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <div className="user-details-extended__orgs">
+                <h3>Organizations:</h3>
+                <ul>
+                    {userOrgs.map(org => (
+                        <li key={org.id}>
+                            <a href={org.html_url}>{org.login}</a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
     );
 }
