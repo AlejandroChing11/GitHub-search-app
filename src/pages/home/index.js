75abbab1b09d647d31dc7fa44945d29c5b9dd9f4
Alejandro Ching <alejandroching2004@hotmail.com>
2023-02-22 10:08:37 -0500

    Adding spinner

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,19 +2,24 @@ import React, { useState } from 'react';
 import axios from "axios";
 import UserCard from '../../components/UserCard';
 import '../../styles/index.css';
+import Spinner from 'react-bootstrap/Spinner';
 
 
 function Home() {
     const [username, setUsername] = useState('');
     const [userData, setUserData] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
 
     const handleSearch = () => {
+        setIsLoading(true);
         axios.get(`https://api.github.com/search/users?q=${username}`)
             .then(response => {
                 setUserData(response.data.items);
+                setIsLoading(false);
             })
             .catch(error => {
                 console.log(error);
+                setIsLoading(false);
             });
     }
 
@@ -27,6 +32,11 @@ function Home() {
             <h1>Github User Search</h1>
             <input type="text" value={username} onChange={handleUsernameChange} />
             <button onClick={handleSearch}>Search</button>
+            {isLoading &&
+                <div className="spinner-container">
+                    <Spinner animation="border" variant="primary" />
+                </div>
+            }
             {userData.length > 0 && (
                 <ul>
                     {userData.map(user => (
